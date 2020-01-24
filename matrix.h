12d3661748008850af33fb97c1d46fb87583561b
shensong <1570467256@qq.com>
2020-01-24 13:03:00 +0800

    initial edition

---
+++ matrix.h
@@ -0,0 +1,21 @@
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+
+#include <vector>
+#include <complex>
+#include "polynomial.h"
+
+namespace song
+{
+    template<class T>
+    class basic_matrix:public std::vector<T>
+    {
+        std::size_t rows,cols;
+    public:
+
+    };
+}
+
+
+
+#endif // MATRIX_H_INCLUDED
